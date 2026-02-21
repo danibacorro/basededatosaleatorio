7eb3ddd683efdc3ba251a74f5cb45e057a2bc48c
danibacorro <danibacorro@gmail.com>
2026-02-21 14:39:30 +0100

    Add files via upload

--- expo_estructura.sql
+++ expo_estructura.sql
@@ -1,103 +1,89 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: prueba
--- ------------------------------------------------------
--- Server version	11.8.3-MariaDB-0+deb13u1 from Debian-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
-
---
--- Table structure for table `bonus`
+-- PostgreSQL database dump
 --
 
-DROP TABLE IF EXISTS `bonus`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bonus` (
-  `ENAME` varchar(10) DEFAULT NULL,
-  `JOB` varchar(9) DEFAULT NULL,
-  `SAL` int(11) DEFAULT NULL,
-  `COMM` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+\restrict okESn1sbaWxpzVLgJ8IdNQqMTllfXp7DywgPJqhBqQGrHm3FToV4uPYKHqngjDc
+
+-- Dumped from database version 17.8 (Debian 17.8-0+deb13u1)
+-- Dumped by pg_dump version 17.8 (Debian 17.8-0+deb13u1)
+
+-- Started on 2026-02-21 14:30:44 CET
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP TABLE IF EXISTS scott.emp;
+DROP TABLE IF EXISTS scott.dept;
+DROP TABLE IF EXISTS scott.bonus;
+DROP SCHEMA IF EXISTS scott;
+--
+-- TOC entry 6 (class 2615 OID 16688)
+-- Name: scott; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA scott;
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
 
 --
--- Table structure for table `dept`
+-- TOC entry 220 (class 1259 OID 16695)
+-- Name: bonus; Type: TABLE; Schema: scott; Owner: -
 --
 
-DROP TABLE IF EXISTS `dept`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `dept` (
-  `deptno` decimal(2,0) DEFAULT NULL,
-  `dname` varchar(14) DEFAULT NULL,
-  `loc` varchar(13) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE scott.bonus (
+    ename character varying(10),
+    job character varying(9),
+    sal numeric,
+    comm numeric
+);
+
 
 --
--- Table structure for table `emp`
+-- TOC entry 218 (class 1259 OID 16689)
+-- Name: dept; Type: TABLE; Schema: scott; Owner: -
 --
 
-DROP TABLE IF EXISTS `emp`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `emp` (
-  `empno` decimal(4,0) NOT NULL,
-  `ename` varchar(10) DEFAULT NULL,
-  `job` varchar(9) DEFAULT NULL,
-  `mgr` decimal(4,0) DEFAULT NULL,
-  `hiredate` date DEFAULT NULL,
-  `sal` decimal(7,2) DEFAULT NULL,
-  `comm` decimal(7,2) DEFAULT NULL,
-  `deptno` decimal(2,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_uca1400_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER trg_audit_emp_comm
-AFTER INSERT ON emp
-FOR EACH ROW
-BEGIN
-IF NEW.comm IS NOT NULL AND NEW.comm > 0 THEN
-    INSERT INTO audit_emp_comm (empno, ename, comm, usuario, fecha)
-    VALUES (NEW.empno, NEW.ename, NEW.comm, USER(), NOW());
-END IF;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+CREATE TABLE scott.dept (
+    deptno numeric(2,0) NOT NULL,
+    dname character varying(14),
+    loc character varying(13)
+);
+
 
 --
--- Dumping routines for database 'prueba'
+-- TOC entry 219 (class 1259 OID 16692)
+-- Name: emp; Type: TABLE; Schema: scott; Owner: -
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+CREATE TABLE scott.emp (
+    empno numeric(4,0) NOT NULL,
+    ename character varying(10),
+    job character varying(9),
+    mgr numeric(4,0),
+    hiredate date,
+    sal numeric(7,2),
+    comm numeric(7,2),
+    deptno numeric(2,0) NOT NULL
+);
 
--- Dump completed on 2026-02-21 13:53:13
+
+-- Completed on 2026-02-21 14:30:44 CET
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict okESn1sbaWxpzVLgJ8IdNQqMTllfXp7DywgPJqhBqQGrHm3FToV4uPYKHqngjDc
+
