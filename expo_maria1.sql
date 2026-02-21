0b9097f32bff91b979811f8e46dc2b7ae923ba1f
danibacorro <danibacorro@gmail.com>
2026-02-21 13:39:33 +0100

    Add files via upload

---
+++ expo_maria1.sql
@@ -0,0 +1,130 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: prueba
+-- ------------------------------------------------------
+-- Server version	11.8.3-MariaDB-0+deb13u1 from Debian-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `dept`
+--
+
+DROP TABLE IF EXISTS `dept`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dept` (
+  `deptno` decimal(2,0) DEFAULT NULL,
+  `dname` varchar(14) DEFAULT NULL,
+  `loc` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dept`
+--
+
+LOCK TABLES `dept` WRITE;
+/*!40000 ALTER TABLE `dept` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `dept` VALUES
+(10,'ACCOUNTING','NEW YORK'),
+(20,'RESEARCH','DALLAS'),
+(30,'SALES','CHICAGO'),
+(40,'OPERATIONS','BOSTON');
+/*!40000 ALTER TABLE `dept` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `emp`
+--
+
+DROP TABLE IF EXISTS `emp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emp` (
+  `empno` decimal(4,0) NOT NULL,
+  `ename` varchar(10) DEFAULT NULL,
+  `job` varchar(9) DEFAULT NULL,
+  `mgr` decimal(4,0) DEFAULT NULL,
+  `hiredate` date DEFAULT NULL,
+  `sal` decimal(7,2) DEFAULT NULL,
+  `comm` decimal(7,2) DEFAULT NULL,
+  `deptno` decimal(2,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emp`
+--
+
+LOCK TABLES `emp` WRITE;
+/*!40000 ALTER TABLE `emp` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `emp` VALUES
+(7369,'SMITH','CLERK',7902,'1980-12-17',800.00,NULL,20),
+(7499,'ALLEN','SALESMAN',7698,'1981-02-20',1600.00,300.00,30),
+(7521,'WARD','SALESMAN',7698,'1981-02-22',1250.00,500.00,30),
+(7566,'JONES','MANAGER',7839,'1981-04-02',2975.00,NULL,20),
+(7654,'MARTIN','SALESMAN',7698,'1981-09-28',1250.00,1400.00,30),
+(7698,'BLAKE','MANAGER',7839,'1981-05-01',2850.00,NULL,30),
+(7782,'CLARK','MANAGER',7839,'1981-06-09',2450.00,NULL,10),
+(7788,'SCOTT','ANALYST',7566,'1982-12-09',3000.00,NULL,20),
+(7839,'KING','PRESIDENT',NULL,'1981-11-17',5000.00,NULL,10),
+(7844,'TURNER','SALESMAN',7698,'1981-09-08',1500.00,0.00,30),
+(7876,'ADAMS','CLERK',7788,'1983-01-12',1100.00,NULL,20),
+(7900,'JAMES','CLERK',7698,'1981-12-03',950.00,NULL,30),
+(7902,'FORD','ANALYST',7566,'1981-12-03',3000.00,NULL,20),
+(7934,'MILLER','CLERK',7782,'1982-01-23',1300.00,NULL,10),
+(8934,'MIL','CLERK',7782,'1982-01-23',1300.00,NULL,10),
+(8499,'ALL','SALESMAN',7698,'1981-02-20',1600.00,300.00,30),
+(6499,'SMITH','SALESMAN',7698,'1981-02-20',1500.00,100.00,40),
+(6499,'SMITH','SALESMAN',7698,'1981-02-20',1500.00,100.00,40);
+/*!40000 ALTER TABLE `emp` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_uca1400_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER trg_audit_emp_comm
+AFTER INSERT ON emp
+FOR EACH ROW
+BEGIN
+IF NEW.comm IS NOT NULL AND NEW.comm > 0 THEN
+    INSERT INTO audit_emp_comm (empno, ename, comm, usuario, fecha)
+    VALUES (NEW.empno, NEW.ename, NEW.comm, USER(), NOW());
+END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-02-21 13:38:30
